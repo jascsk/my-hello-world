4371038e62c5535353e81f7fd9c4eb9f5a5006a8
sc186002 <sc186002@WINSC186002-YSA.TD.TERADATA.COM>
2016-07-31 13:17:23 +0530

    initial commit.. project created

---
+++ Sample1_Team1/src/com/teradata/scrum/training/team1/SampleDemo.java
@@ -0,0 +1,11 @@
+package com.teradata.scrum.training.team1;
+
+public class SampleDemo {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This is from team1 contribution");
+	}
+	
+}
