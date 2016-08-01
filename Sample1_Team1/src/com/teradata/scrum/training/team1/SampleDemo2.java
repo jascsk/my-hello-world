669d9deacfe065aeb7cb6d06cc977a0d0946e1ef
sc186002 <sc186002@WINSC186002-YSA.TD.TERADATA.COM>
2016-08-01 18:54:25 +0530

    new file added

---
+++ Sample1_Team1/src/com/teradata/scrum/training/team1/SampleDemo2.java
@@ -0,0 +1,11 @@
+package com.teradata.scrum.training.team1;
+
+public class SampleDemo2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("THis is another calls newly added");
+
+	}
+
+}
